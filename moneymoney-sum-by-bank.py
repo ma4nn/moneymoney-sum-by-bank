4af6bf0ae5c7110bc94ade4f4ef97df0e0844672
Christoph Massmann <cm@vianetz.com>
2024-01-05 10:26:54 +0100

    minor code style improvements

--- moneymoney-sum-by-bank.py
+++ moneymoney-sum-by-bank.py
@@ -1,17 +1,22 @@
-import pandas as pd
+#!/usr/bin/env python3
+"""
+  Python script for the great MoneyMoney app to generate a list with total balance by bank account.
+"""
+
+import logging
 import plistlib
 import subprocess
-import logging
+import pandas as pd
 
 
-def run_apple_script(script):
+def run_apple_script(script: str) -> bytes:
     command = ['osascript', '-e', script]
     with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as pipe:
         result = pipe.communicate()
         if result[1]:
-            raise Exception('Could not run Apple Script: %s' % result[1].decode().strip())
+            raise RuntimeError(f'Could not run Apple Script "{script}": {result[1].decode().strip()}')
 
-        return result
+        return result[0]
 
 
 # @see https://moneymoney-app.com/applescript/
@@ -20,9 +25,9 @@ def fetch_moneymoney_accounts() -> {}:
 
     # Parse XML property list.
     try:
-        plist = plistlib.loads(result[0])
+        plist = plistlib.loads(result)
     except plistlib.InvalidFileException as exception:
-        raise Exception('Could not parse XML property list. %s' % repr(exception))
+        raise ValueError('Could not parse XML property list') from exception
 
     return plist
 
@@ -35,15 +40,17 @@ def moneymoney_sum_by_account() -> pd.DataFrame:
 
         if not account['bankCode']:
             account['bankCode'] = account['attributes']['bankIdentifier'] if 'bankIdentifier' in account['attributes'] else 'other'
-            logging.debug("Account %s has no bank code, using '%s'" % (account["name"], account['bankCode']))
+            logging.debug("Account %s has no bank code, using '%s'", account["name"], account['bankCode'])
 
         for balance in account['balance']:
             balance_per_bank_and_currency.append([account['bankCode'], balance[0], balance[1]])
 
-    df = pd.DataFrame(balance_per_bank_and_currency, columns=['bank', 'balance', 'currency'])
-    return df.groupby(['bank', 'currency']).agg({'balance': 'sum'})
+    df_balance_per_bank_and_currency = pd.DataFrame(balance_per_bank_and_currency, columns=['bank', 'balance', 'currency'])
+
+    return df_balance_per_bank_and_currency.groupby(['bank', 'currency']).agg({'balance': 'sum'})
 
 
-#logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-df = moneymoney_sum_by_account()
-print(df[df['balance'] > 0].sort_values(by='balance', ascending=False))
+if __name__ == "__main__":
+    # logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    df = moneymoney_sum_by_account()
+    print(df[df['balance'] > 0].sort_values(by='balance', ascending=False))
